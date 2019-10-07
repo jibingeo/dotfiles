1c22fed097c0ba932bf0c31a964452ed29681bfa
jibin george <hello@jibingeo.com>
2019-10-07 09:49:21 +0530

    rewrite to stow

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-source ~/.profile;clear;
-
-if status --is-interactive
-    abbr --add --global gs git status
-    abbr --add --global gco git checkou
-    abbr --add --global n nvim 
-    abbr --add --global vim nvim 
-end
