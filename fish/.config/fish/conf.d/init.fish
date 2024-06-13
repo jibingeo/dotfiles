ac85327d496ee60181bf8a03ed21052425efea97
Jibin George <hello@jibingeo.com>
2024-06-13 10:23:37 +0530

    Added onmyposh, Disable startship

--- fish/.config/fish/conf.d/init.fish
+++ fish/.config/fish/conf.d/init.fish
@@ -1,8 +1,15 @@
 set fish_greeting
 
+# Add newline after every prompt
 function add_newline --on-event fish_prompt
     echo
 end
 
 # starship init
-starship init fish | source
+# starship init fish | source
+# enable_transience
+
+# Oh My Posh
+oh-my-posh init fish --config ~/.config/ohmyposh/config.toml | source
+
+zoxide init fish | source
